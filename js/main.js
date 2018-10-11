3b120b423a2fb8bff9789f9fece285885443bf6f
Truvam <truvam@users.noreply.github.com>
2018-10-11 16:59:24 +0100

    Added mouseover and mouseout events

--- js/main.js
+++ js/main.js
@@ -4,25 +4,63 @@ class Board {
         this.height = height;
         this.class = _class;
         this.create_board();
+        this.event_listener();
     }
 
     create_board() {
-        var board = document.getElementsByClassName(this.class);
+        const board = document.getElementsByClassName(this.class)[0];
         for(let i = 0; i < this.height; i++) {
-            var row = document.createElement('div');
+            const row = document.createElement('div');
             row.className = 'row';
-            board[0].appendChild(row);
+            board.appendChild(row);
             for(let j = 0; j < this.width; j++) {
-                var columns = document.createElement('div');
-                columns.className = 'cell';
+                const columns = document.createElement('div');
+                columns.className = 'cell empty';
+                columns.dataset.column = j;
+                columns.dataset.row = i;
                 row.appendChild(columns);
             }
         }
     }
+
+    event_listener() {
+        const board = document.getElementsByClassName(this.class)[0];
+
+        function find_empty_cell(column) {
+            const cells = document.querySelectorAll('[data-column="' + column + '"]');
+            for(let i = cells.length - 1; i >= 0; i--) {
+                if(cells[i].className.includes('empty')) {
+                    console.log("cells", cells[i]);
+                    return cells[i];
+                }
+            }
+            return null;
+        }
+
+        board.addEventListener('mouseover', (event) => { 
+            if (event.target.className.includes('cell empty')) {
+                const column = event.target.dataset.column;
+                console.log("col", column);
+                const empty_cell = find_empty_cell(column);
+                empty_cell.classList.add('cell-blue');
+            }
+        });
+
+        board.addEventListener('mouseout', (event) => { 
+            if (event.target.className.includes('cell')) {
+                const cells = document.querySelectorAll('.cell-blue')[0];
+                console.log("Name: ", cells);
+                cells.classList.remove('cell-blue');
+                console.log(event.target.classList);
+            }
+        });
+    }
+
 }
 
+
 window.onload = function() {
-    width = document.getElementById('size-w').value;
-    height = document.getElementById('size-h').value;
+    const width = document.getElementById('size-w').value;
+    const height = document.getElementById('size-h').value;
     const board = new Board(width, height,'board');
 };
