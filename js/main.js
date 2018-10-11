29314bbacc5333bc043113dd1166e6082d930004
Truvam <truvam@users.noreply.github.com>
2018-10-11 19:09:41 +0100

    Started Player vs Player mode and separated board class

--- js/main.js
+++ js/main.js
@@ -1,73 +1,3 @@
-class Board {
-    constructor(width, height, _class) {
-        this.width = width;
-        this.height = height;
-        this.class = _class;
-        this.create_board();
-    }
-
-    create_board() {
-        const board = document.getElementsByClassName(this.class)[0];
-        for (let i = 0; i < this.height; i++) {
-            const row = document.createElement('div');
-            row.className = 'row';
-            board.appendChild(row);
-            for (let j = 0; j < this.width; j++) {
-                const columns = document.createElement('div');
-                columns.className = 'cell empty';
-                columns.dataset.column = j;
-                columns.dataset.row = i;
-                row.appendChild(columns);
-            }
-        }
-    }
-
-    event_listener() {
-        const board = document.getElementsByClassName(this.class)[0];
-
-        function find_empty_cell(column) {
-            const cells = document.querySelectorAll('[data-column="' + column + '"]');
-            for (let i = cells.length - 1; i >= 0; i--) {
-                if (cells[i].className.includes('empty')) {
-                    console.log("cells", cells[i]);
-                    return cells[i];
-                }
-            }
-            return null;
-        }
-
-        board.addEventListener('mouseover', (event) => { 
-            if (event.target.className.includes('cell empty')) {
-                const column = event.target.dataset.column;
-                console.log("col", column);
-                const empty_cell = find_empty_cell(column);
-                empty_cell.classList.add('cell-red');
-            }
-        });
-
-        board.addEventListener('mouseout', (event) => { 
-            if (event.target.className.includes('cell')) {
-                const cells = document.querySelectorAll('.cell-red')[0];
-                console.log("Name: ", cells);
-                cells.classList.remove('cell-red');
-                console.log(event.target.classList);
-            }
-        });
-
-        board.addEventListener('click', (event) => { 
-            if (event.target.className.includes('cell empty')) {
-                const column = event.target.dataset.column;
-                const empty_cell = find_empty_cell(column);
-                empty_cell.classList.remove('empty');
-                empty_cell.classList.add('red');
-            }
-        });
-
-    }
-
-}
-
-
 window.onload = function() {
     const width = document.getElementById('size-w').value;
     const height = document.getElementById('size-h').value;
@@ -95,6 +25,7 @@ function start_game() {
     const width = document.getElementById('size-w').value;
     const height = document.getElementById('size-h').value;
     const board = new Board(width, height,'board');
+    document.getElementsByClassName("board")[0].style.opacity = "1";
     board.event_listener();
 }
 
