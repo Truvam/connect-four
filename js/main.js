2a23a8538aa664e8d4d1f48f5948f95a6b7a09ee
Truvam <truvam@users.noreply.github.com>
2018-11-21 17:17:17 +0000

    Implemented function to join two players

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ window.onload = function () {
                 set_current_player(board.second_player, "blue");
                 play_ai(board);
             }
-        } else {
+        } else if (opponent == 'player') {
             if (document.getElementById('first-player').checked) {
                 board.first_player = document.getElementById('first-player').value;
                 board.second_player = document.getElementById('second-player').value;
@@ -42,6 +42,12 @@ window.onload = function () {
                 board.second_player = document.getElementById('first-player').value;
                 set_current_player(board.first_player, "red");
             }
+        } else if (opponent == 'online') {
+            const nick = localStorage.getItem('nick');
+            const pass = localStorage.getItem('pass');
+            document.getElementsByClassName("user-logout")[0].innerHTML = nick;
+            console.log("Nick:", nick, "Pass:", pass);
+            join(4623, nick, pass, {'rows':Number(height), 'columns':Number(width)});
         }
         board.current_player = board.first_player;
         document.getElementsByClassName("board")[0].style.opacity = "1";
@@ -49,14 +55,13 @@ window.onload = function () {
     } else {
         new Board(width, height, 'board');
         let leaderboard = {};
-        if(!localStorage.getItem('first')) {
+        if (!localStorage.getItem('first')) {
             localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
             localStorage.setItem('first', true);
-        }
-        else if (localStorage.getItem('first')){
+        } else if (localStorage.getItem('first')) {
             leaderboard = localStorage.getItem('leaderboard');
             insert_leaderboard('', 1);
-        } 
+        }
         document.getElementsByClassName("configuration")[0].style.display = "none";
         document.getElementsByClassName("board")[0].style.display = "none";
         document.getElementsByClassName("logout")[0].style.display = "none";
@@ -109,8 +114,17 @@ function start_game() {
         }
         const diff = document.querySelectorAll('input[name="config-dif"]:checked')[0].id;
         localStorage.setItem('diff', diff);
-    } else {
+    } else if (document.getElementById('player').checked) {
         localStorage.setItem('opponent', 'player');
+        if (document.getElementById('first-player').checked) {
+            localStorage.setItem('first_player', 'first-player');
+        } else {
+            localStorage.setItem('first_player', 'second-player');
+        }
+    } else if (document.getElementById('online').checked) {
+        localStorage.setItem('nick', document.getElementById('user').value);
+        localStorage.setItem('pass', document.getElementById('pass').value);
+        localStorage.setItem('opponent', 'online');
         if (document.getElementById('first-player').checked) {
             localStorage.setItem('first_player', 'first-player');
         } else {
@@ -160,22 +174,19 @@ function show_leaderboard() {
 
 function quit_game(op) {
     close_panels();
-    if(op == "yes") {
-        who_won(board, quit=true);
-    }
-    else if(op == 'show') {
+    if (op == "yes") {
+        who_won(board, quit = true);
+    } else if (op == 'show') {
         document.getElementsByClassName("quit")[0].style.display = "unset";
-    }
-    else {
+    } else {
         document.getElementsByClassName("quit")[0].style.display = "none";
     }
 }
 
 function set_current_player(current_player, color) {
-    if(document.getElementsByClassName("who-won")[0].style.display == "unset") {
+    if (document.getElementsByClassName("who-won")[0].style.display == "unset") {
         document.getElementsByClassName("current-player")[0].style.display = "none";
-    }
-    else {
+    } else {
         document.getElementsByClassName("current-player")[0].style.display = "unset";
         document.getElementsByClassName("current-player")[0].innerHTML = current_player;
         if (color == "red") {
@@ -193,21 +204,20 @@ function insert_leaderboard(player, start) {
     leaderboard = JSON.parse(leaderboard)
     const table = document.getElementById("table");
     const table_values = document.getElementsByClassName("table-values");
-    for(let i = 0; i < table_values.length; i++) {
+    for (let i = 0; i < table_values.length; i++) {
         table_values[i].innerHTML = "";
     }
 
-    if(start == 0) {
-        if(!leaderboard.hasOwnProperty(player)) {
+    if (start == 0) {
+        if (!leaderboard.hasOwnProperty(player)) {
             leaderboard[player] = 1
             console.log(leaderboard[player]);
-        }
-        else {
+        } else {
             leaderboard[player]++;
         }
-    }    
-    
-    for(let key in leaderboard) {
+    }
+
+    for (let key in leaderboard) {
         const tr = document.createElement('tr');
         tr.className = "table-values";
         let td = document.createElement('td');
@@ -225,32 +235,35 @@ function insert_leaderboard(player, start) {
 }
 
 function set_onclick_events() {
-    document.getElementById('login-button').addEventListener('click', function(event) {
+    document.getElementById('login-button').addEventListener('click', function (event) {
         const nick = document.getElementById('user').value;
         const pass = document.getElementById('pass').value;
         register(nick, pass);
     });
-    document.getElementById('player').addEventListener('click', function(event) {
+    document.getElementById('player').addEventListener('click', function (event) {
         select_opponent('player');
     });
-    document.getElementById('ai').addEventListener('click', function(event) {
+    document.getElementById('ai').addEventListener('click', function (event) {
         select_opponent('ai');
+    });
+    document.getElementById('online').addEventListener('click', function (event) {
+        select_opponent('online');
     });
     document.getElementsByClassName('config-btn')[0].onclick = start_game;
     document.getElementsByClassName('btn-start')[0].onclick = start_game;
     document.getElementsByClassName('btn-config')[0].onclick = show_config;
     document.getElementsByClassName('btn-rules')[0].onclick = show_rules;
     document.getElementsByClassName('btn-leaderboard')[0].onclick = show_leaderboard;
-    document.getElementsByClassName('btn-quit')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('btn-quit')[0].addEventListener('click', function (event) {
         quit_game('show');
     });
-    document.getElementsByClassName('no-btn')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('no-btn')[0].addEventListener('click', function (event) {
         quit_game('no');
     });
-    document.getElementsByClassName('yes-btn')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('yes-btn')[0].addEventListener('click', function (event) {
         quit_game('yes');
     });
-    document.getElementsByClassName('btn-logout')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('btn-logout')[0].addEventListener('click', function (event) {
         window.location.reload();
     });
 
--- js/server.js
+++ js/server.js
@@ -26,7 +26,19 @@ function register(nick, pass) {
 }
 
 function join(group, nick, pass, size) {
-
+    console.log(size);
+    fetch(url+'join', {
+        method: 'post',
+        body: JSON.stringify({'group':group, 'nick':nick, 'pass':pass, 'size':size})
+    })
+    .then(response => response.json())
+    .then(function(response) {
+        console.log('response: ', response);
+        if(response.hasOwnProperty('game')) {
+            update(nick, response.game);
+            notify(nick, pass, response.game, 1);
+        }
+    });
 }
 
 function leave(nick, pass, size) {
@@ -34,15 +46,25 @@ function leave(nick, pass, size) {
 }
 
 function notify(nick, pass, game, column) {
-
-}
-
-function notify(nick, pass, game, column) {
-
+    console.log("Notify");
+    fetch(url+'notify', {
+        method: 'post',
+        body: JSON.stringify({'nick':nick, 'pass':pass, 'game':game, 'column':column})
+    })
+    .then(response => response.json())
+    .then(function(response) {
+        console.log('response: ', response);
+    });
 }
 
 function update(nick, game) {
-
+    const eventSource = new EventSource(url + 'update?nick=' + nick + '&game=' + game);
+    console.log("OK:", eventSource);
+    eventSource.onmessage = function(event) {
+        var data = JSON.parse(event.data);
+        console.log("onmessage:", data);
+    }
+    //eventSource.close();
 }
 
 function ranking(size) {
