1d73ea72391dea9c54828eff25badd9c986a7bd7
Truvam <truvam@users.noreply.github.com>
2018-12-19 09:55:23 +0000

    Added basic encryption to passwords

--- backend/register.js
+++ backend/register.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const fs = require('fs');
+const crypto = require('crypto');
 
 function register(body, callback) {
     let answer = {}
@@ -19,7 +20,7 @@ function verify_login(nick, pass) {
     const data = fs.readFileSync('passwd.json');
     let users = JSON.parse(data);
     if (users.hasOwnProperty(nick)) {
-        if (users[nick] != pass) {
+        if (decipher(users[nick]) != pass) {
             answer.error = "User registered with a different password";
             answer.status = 401;
         }
@@ -28,7 +29,7 @@ function verify_login(nick, pass) {
             answer.json = {};
         }
     } else {
-        users[nick] = pass;
+        users[nick] = cipher(pass);
         answer.status = 200;
         answer.json = {};
     }
@@ -36,4 +37,20 @@ function verify_login(nick, pass) {
     const json = JSON.stringify(users);
     fs.writeFileSync('passwd.json', json);
     return answer;
+}
+
+function cipher(value) {
+    let key = crypto.createCipher('aes-128-cbc', 'G)yNT@d]}#zz"`9%');
+    let hash = key.update(value, 'utf8', 'hex')
+    hash += key.final('hex');
+
+    return hash
+}
+
+function decipher(hash) {
+    let key = crypto.createDecipher('aes-128-cbc', 'G)yNT@d]}#zz"`9%');
+    let value = key.update(hash, 'hex', 'utf8')
+    value += key.final('utf8');
+
+    return value
 }
