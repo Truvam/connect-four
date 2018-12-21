93935125522d37616375902ec1372704f4b99a44
Truvam <truvam@users.noreply.github.com>
2018-12-21 10:34:12 +0000

    Fixed join to work with the updater module

--- backend/join.js
+++ backend/join.js
@@ -2,9 +2,10 @@
 
 const crypto = require('crypto');
 
-function join(body, callback) {
+function join(body, game_info, callback) {
     let answer = {};
 
+    if(game_info.hasOwnProperty('game')) body.game = game_info.game;
     answer = create_hash(body);
     answer.style = 'json';
     console.log("Rans: ", answer);
@@ -15,18 +16,21 @@ module.exports.join = join;
 
 function create_hash(body) {
     let answer = {};
-    
-    try {
-        body.time = Date.now();
-        let hash = crypto.createHash('md5').update(JSON.stringify(body)).digest("hex");
-
-        answer.json = {"game": hash};
+    if(body.hasOwnProperty('game')) {
+        answer.json = {"game": body.game};
         answer.status = 200;
-    } catch (error) {
-        answer.error = "Unable to create game hash";
-        answer.status = 400;
     }
-    
+    else {
+        try {
+            body.time = Date.now();
+            let hash = crypto.createHash('md5').update(JSON.stringify(body)).digest("hex");
+            answer.json = {"game": hash};
+            answer.status = 200;
+        } catch (error) {
+            answer.error = "Unable to create game hash";
+            answer.status = 400;
+        }
+    }
 
     return answer;
 }
