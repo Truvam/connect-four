d0d6cf9500b6308856467655a875bbfd87daee4e
Truvam <truvam@users.noreply.github.com>
2019-01-02 13:11:31 +0000

    Added trycatch to leave, notify and ranking modules

--- backend/leave.js
+++ backend/leave.js
@@ -3,21 +3,27 @@ const ranking = require('./ranking.js');
 module.exports.leave = function (body, game_info, callback) {
     let answer = {};
 
-    if(game_info.players < 2) {
-        answer.winner = {"winner": null};
-        answer.style = 'sse';
+    try {
+        if(game_info.players < 2) {
+            answer.winner = {"winner": null};
+            answer.style = 'sse';
+        }
+        else {
+            if(body.nick == game_info.nick[0]) answer.winner = {"winner": game_info.nick[1]};
+            else answer.winner = {"winner": game_info.nick[0]};
+            game_info.winner = answer.winner.winner;
+            ranking.insert_ranking(game_info);
+            answer.style = 'json';
+        }
+    
+        answer.status = 200;
+        answer.json = {};
+        console.log("LEans: ", answer);
+    } catch (error) {
+        answer.status = 500;
+        answer.error = 'Unable to leave game';
     }
-    else {
-        if(body.nick == game_info.nick[0]) answer.winner = {"winner": game_info.nick[1]};
-        else answer.winner = {"winner": game_info.nick[0]};
-        game_info.winner = answer.winner.winner;
-        ranking.insert_ranking(game_info);
-        answer.style = 'json';
-    }
-
-    answer.status = 200;
-    answer.json = {};
-    console.log("LEans: ", answer);
+    
 
     callback(answer);
 }
--- backend/notify.js
+++ backend/notify.js
@@ -3,19 +3,25 @@
 module.exports.notify = function (body, game_info, callback) {
     let answer = {};
 
-    if (body.nick != game_info.turn) {
-        answer.error = "Not your turn to play";
-        answer.status = 401;
-    } else if (body.column < 0) {
-        answer.error = "Column reference is negative";
-        answer.status = 401;
-    } else {
-        answer.json = {};
-        answer.status = 200;
-        game_info.column = body.column;
-        if (game_info.turn == game_info.nick[0]) game_info.turn = game_info.nick[1];
-        else game_info.turn = game_info.nick[0];
+    try {
+        if (body.nick != game_info.turn) {
+            answer.error = "Not your turn to play";
+            answer.status = 401;
+        } else if (body.column < 0) {
+            answer.error = "Column reference is negative";
+            answer.status = 401;
+        } else {
+            answer.json = {};
+            answer.status = 200;
+            game_info.column = body.column;
+            if (game_info.turn == game_info.nick[0]) game_info.turn = game_info.nick[1];
+            else game_info.turn = game_info.nick[0];
+        }
+    } catch (error) {
+        answer.status = 500;
+        answer.error = 'Unable to notify';
     }
+    
 
     answer.style = 'json';
     console.log("NOans: ", answer);
